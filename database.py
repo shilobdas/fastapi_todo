d54c378b3c074aaca7d1405ca64112451fa5029f
shilobdas <drogonjang@gmail.com>
2026-02-10 15:25:35 +0600

    first commit

--- database.py
+++ database.py
@@ -1,28 +1,11 @@
-# from sqlalchemy import create_engine
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker
-
-# DATABASE_URL = "postgresql://postgres:dhaka1219@localhost:5432/todo_db"
-
-# engine = create_engine(DATABASE_URL)
-# SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-# Base = declarative_base()
-
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "postgresql://postgres:dhaka1219@localhost:5432/todo_db"
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
 
 def get_db():
